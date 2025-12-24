d7d52b743bf80e3f1a84a0f0915810b1ec610b4b
Hai Hoang <doannhuy17062005@gmail.com>
2025-12-24 19:03:53 +0700

    Update vite config base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/booking/",
-  plugins: [react()],
+  base: "/doannhuy17062005-cmyk.github.io/",
   server: {
     port: 5173,
     proxy: {
