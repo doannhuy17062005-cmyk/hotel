944f115f9452cf102ddacf6bc293dbbf72b50dd6
Hai Hoang <doannhuy17062005@gmail.com>
2026-01-17 10:39:58 +0700

    Fix vite install

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/hotel/' : '/',   // ✅ dev /, deploy /hotel/
+  base: '/',   // ✅ dev /, deploy /hotel/
   plugins: [react()],
   server: {
     port: 5173,
